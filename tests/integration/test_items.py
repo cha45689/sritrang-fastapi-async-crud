393f25e60df507b7c20fa52e2f42447d261de791
ไชยพร บุญญาเสถียร <chaiya45689@gmail.com>
2024-12-23 20:41:42 +0700

    Add placeholder to test pytest on githhub actions

---
+++ tests/integration/test_items.py
@@ -0,0 +1,33 @@
+"""
+This module is use to store integration test code for items route
+under path api/v1/items
+"""
+import pytest
+
+
+@pytest.mark.asyncio
+async def test_create():
+    """
+    Test POST method of items routes aka create item
+    """
+
+
+@pytest.mark.asyncio
+async def test_read():
+    """
+    Test GET method of items routes aka read item
+    """
+
+
+@pytest.mark.asyncio
+async def test_update():
+    """
+    Test PUT method of items routes aka update item
+    """
+
+
+@pytest.mark.asyncio
+async def test_delete():
+    """
+    Test DELETE method of items routes
+    """
