578bbad19d0e554ee65ae51034668f5403f82d38
ไชยพร บุญญาเสถียร <chaiya45689@gmail.com>
2024-12-23 22:30:10 +0700

    Add Item ORM and model

--- app/database/models/items.py
+++ app/database/models/items.py
@@ -1,7 +1,6 @@
 """
 This module is use to declare Item model and relate model
 """
-
 from tortoise import fields, models
 from tortoise.contrib.pydantic import pydantic_model_creator
 
@@ -10,11 +9,10 @@ class ItemsORM(models.Model):
     """
     Base Item model for ORM and later generate pydantic model
     """
-
     id = fields.IntField(primary_key=True)
     name = fields.CharField(max_length=255)
     description = fields.CharField(max_length=65535)
 
 
-ItemInput: type = pydantic_model_creator(ItemsORM, exclude_readonly=True)
-Item: type = pydantic_model_creator(ItemsORM)
+ItemInput = pydantic_model_creator(ItemsORM, exclude_readonly=True)
+Item = pydantic_model_creator(ItemsORM)
