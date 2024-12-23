e712716a87be3102c4ed070866c51b8d542ede12
ไชยพร บุญญาเสถียร <chaiya45689@gmail.com>
2024-12-23 21:06:43 +0700

    Add unittest place holder to test caching

---
+++ tests/unit/data_model/test_item.py
@@ -0,0 +1,15 @@
+"""
+This file is use to store unit test code for item data model
+"""
+
+
+def test_valid_item():
+    """
+    Test valid item data
+    """
+
+
+def test_invalid_item():
+    """
+    Test invalid item data
+    """
