2c67a4f05b21bc126bec1bcc70def5b729a447a4
ไชยพร บุญญาเสถียร <chaiya45689@gmail.com>
2024-12-24 23:29:59 +0700

    Add get all with pagination route

--- app/application/routers/items.py
+++ app/application/routers/items.py
@@ -8,7 +8,9 @@ from typing import Annotated
 from fastapi import APIRouter, Depends
 
 from app.application.dependencies.items import init_item
+from app.application.dependencies.pagination import PaginationQuery
 from app.application.exceptions import DefaultHTTPExceptionContent
+from app.application.response import ItemsPaginationContent
 from app.database.models.items import Item, ItemInput, ItemsORM
 
 item_routes = APIRouter(prefix="/items")
@@ -19,10 +21,27 @@ async def create_item(item_payload: ItemInput) -> Item:  # type: ignore[valid-ty
     """
     function use to create item record
     """
-    item: ItemsORM = await ItemsORM.create(
+    item = await ItemsORM.create(
         **item_payload.model_dump()  # type: ignore[attr-defined]
     )
     return item
+
+
+@item_routes.get("/", responses={404: {"model": DefaultHTTPExceptionContent}})
+async def get_items(
+    paginatioon_query: Annotated[PaginationQuery, Depends(PaginationQuery)]
+) -> ItemsPaginationContent:  # type: ignore[valid-type]
+    """
+    function use to create item record
+    """
+    items_queryset = ItemsORM.all()
+    pagination = paginatioon_query.build_pagination(total=await items_queryset.count())
+    items = (
+        await items_queryset.order_by(paginatioon_query.sort + "id")
+        .offset(paginatioon_query.offset)
+        .limit(paginatioon_query.size)
+    )
+    return ItemsPaginationContent(data=items, pagination=pagination)
 
 
 @item_routes.get("/{item_id}", responses={404: {"model": DefaultHTTPExceptionContent}})
