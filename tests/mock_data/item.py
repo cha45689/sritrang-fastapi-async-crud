4e87020918630af3031fbf94d1653bdf35f05613
ไชยพร บุญญาเสถียร <chaiya45689@gmail.com>
2024-12-25 03:15:43 +0700

    Add unit test to check for validation of Item

--- tests/mock_data/item.py
+++ tests/mock_data/item.py
@@ -5,7 +5,7 @@ This is for fixture of mock item data and other mock data for item
 import factory
 import pytest
 
-from app.database.models.items import ItemInput
+from app.database.models.items import Item, ItemInput
 
 
 class ItemInputFactory(factory.Factory):
@@ -22,6 +22,21 @@ class ItemInputFactory(factory.Factory):
 
     name = factory.Faker("pystr", min_chars=0, max_chars=255)
     description = factory.Faker("pystr", min_chars=0, max_chars=65535)
+
+
+class ItemFactory(ItemInputFactory):
+    """
+    Factory for creating Item Object
+    """
+
+    class Meta:  # pylint: disable=too-few-public-methods
+        """
+        Define base object to create in this case Item
+        """
+
+        model = Item
+
+    id = factory.Faker("pyint")
 
 
 @pytest.fixture()
@@ -43,3 +58,18 @@ invalid_item_input = [
 ]
 
 missing_item_input = [{"name": "John"}, {"description": "John"}]
+
+invalid_item = [
+    {"id": 1, "name": 1, "description": 2},
+    {"id": 1.0, "name": 1.5, "description": 2.5},
+    {"id": [1], "name": ["s"], "description": ["a"]},
+    {"id": None, "name": None, "description": None},
+    {"id": {"c"}, "name": {"a"}, "description": {"b"}},
+    {"id": {"c": "d"}, "name": {"a": "b"}, "description": {"b": "c"}},
+    {"id": 1, "name": " " * 256, "description": " " * 65536},
+]
+missing_item = [
+    {"id": 1, "name": "John"},
+    {"name": "John", "description": "John"},
+    {"id": 1, "description": "John"},
+]
