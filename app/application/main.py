8c6da58c2a6d7d0e855929d0de23a6c982998408
ไชยพร บุญญาเสถียร <chaiya45689@gmail.com>
2024-12-24 16:56:55 +0700

    Add create item test cases and handle validation error

--- app/application/main.py
+++ app/application/main.py
@@ -5,7 +5,9 @@ This Module is use to declare and bundle the main FastAPI application
 from contextlib import asynccontextmanager
 
 from fastapi import FastAPI
+from fastapi.exceptions import RequestValidationError
 
+from app.application.exceptions import validation_exception_handler
 from app.application.routers.routes import routers
 from app.database.connection.sqlite import register_sqlite
 
@@ -22,3 +24,5 @@ async def lifespan(app: FastAPI):  # pylint: disable=W0621
 app = FastAPI(lifespan=lifespan)
 
 app.include_router(routers)
+
+app.add_exception_handler(RequestValidationError, validation_exception_handler)
