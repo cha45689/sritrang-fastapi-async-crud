20b5ec7369ac88257dec3ae16036417ad501d001
ไชยพร บุญญาเสถียร <chaiya45689@gmail.com>
2024-12-25 03:15:43 +0700

    Add unit test to check for validation of Item

--- tests/unit/data_model/test_item.py
+++ tests/unit/data_model/test_item.py
@@ -5,15 +5,18 @@ This file is use to store unit test code for item data model
 import pytest
 from pydantic import ValidationError
 
-from app.database.models.items import ItemInput
+from app.database.models.items import Item, ItemInput
 from tests.mock_data.item import (
+    ItemFactory,
     ItemInputFactory,
+    invalid_item,
     invalid_item_input,
+    missing_item,
     missing_item_input,
 )
 
 
-def test_valid_item():
+def test_valid_item_input():
     """
     Test valid item data
     """
@@ -21,7 +24,7 @@ def test_valid_item():
 
 
 @pytest.mark.parametrize("item_input", invalid_item_input)
-def test_invalid_item(item_input):
+def test_invalid_item_input(item_input):
     """
     Test invalid item data
     """
@@ -35,12 +38,47 @@ def test_invalid_item(item_input):
 
 
 @pytest.mark.parametrize("item_input", missing_item_input)
-def test_require_field_item(item_input):
+def test_require_field_item_input(item_input):
     """
     Test invalid item data
     """
     try:  # noqa: SIM105
         ItemInput(**item_input)
+        raise Exception(  # pylint: disable=broad-exception-raised
+            "Not raise expect error"
+        )
+    except ValidationError:
+        pass
+
+
+def test_valid_item():
+    """
+    Test valid item data
+    """
+    ItemFactory.build()
+
+
+@pytest.mark.parametrize("item", invalid_item)
+def test_invalid_item(item):
+    """
+    Test invalid item data
+    """
+    try:  # noqa: SIM105
+        ItemFactory.build(**item)
+        raise Exception(  # pylint: disable=broad-exception-raised
+            "Not raise expect error"
+        )
+    except ValidationError:
+        pass
+
+
+@pytest.mark.parametrize("item", missing_item)
+def test_require_field_item(item):
+    """
+    Test invalid item data
+    """
+    try:  # noqa: SIM105
+        Item(**item)
         raise Exception(  # pylint: disable=broad-exception-raised
             "Not raise expect error"
         )
