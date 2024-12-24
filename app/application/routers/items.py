987f722b75f8bc10a313dcb588c8ad12d8ec16e4
ไชยพร บุญญาเสถียร <chaiya45689@gmail.com>
2024-12-24 20:25:08 +0700

    Add get item by id and update item by id

--- app/application/routers/items.py
+++ app/application/routers/items.py
@@ -3,8 +3,11 @@ This file is use to define all route and controller under items
 ex. {prefix}/items or {prefix}/items/*
 """
 
-from fastapi import APIRouter
+from typing import Annotated
 
+from fastapi import APIRouter, Depends
+
+from app.application.dependencies.items import init_item
 from app.database.models.items import Item, ItemInput, ItemsORM
 
 item_routes = APIRouter(prefix="/items")
@@ -18,4 +21,26 @@ async def create_item(item_payload: ItemInput) -> Item:  # type: ignore[valid-ty
     item: ItemsORM = await ItemsORM.create(
         **item_payload.model_dump()  # type: ignore[attr-defined]
     )
+    return item
+
+
+@item_routes.get("/{item_id}")
+async def get_item(item: Annotated[ItemsORM, Depends(init_item)]) -> Item:  # type: ignore[valid-type]
+    """
+    function use to create item record
+    """
+    return item
+
+
+@item_routes.put("/{item_id}")
+async def update_item(
+    item_payload: ItemInput, item: Annotated[ItemsORM, Depends(init_item)]  # type: ignore[valid-type]
+) -> Item:  # type: ignore[valid-type]
+    """
+    function use to create item record
+    """
+    item = item.update_from_dict(
+        item_payload.model_dump()  # type: ignore[attr-defined]
+    )
+    await item.save()
     return item
